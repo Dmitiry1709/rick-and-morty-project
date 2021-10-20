50f6e4ee091aa878936c54e2bbd4d137e4c931d8
Dmitriy Petrovichev <dimapetrich17@gmail.com>
2021-10-20 22:33:43 +0300

    fix bag

--- src/hooks/useObserver.js
+++ src/hooks/useObserver.js
@@ -10,6 +10,7 @@ export const useObserver = (ref, state, dispatch) => {
         const callback = function(entries, observer) {
             if(entries[0].isIntersecting ){
                 dispatch({type: OBSERVER_START})
+                if(!state.info.next) return null
                 fetch(state.info.next)
                     .then(response => response.json())
                     .then(response => {
