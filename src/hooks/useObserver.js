de251b98db74ea00ec952d5c9cc4dae24b7f773d
Dmitriy Petrovichev <dimapetrich17@gmail.com>
2021-10-21 22:21:50 +0300

    fix errors

--- src/hooks/useObserver.js
+++ src/hooks/useObserver.js
@@ -4,10 +4,10 @@ export const useObserver = (ref, callback, deps = [], info = true) => {
     const observer = useRef()
     useEffect(() => {
         if (!info || !info.next) return;
-        if (observer.current) observer.current.disconnect()
 
         const observerFunc = function (entries, observer) {
             if (entries[0].isIntersecting) {
+                observer.disconnect()
                 callback()
             }
         }
